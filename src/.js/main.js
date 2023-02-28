e5ce05cf9af7dcb175d6678a8db2a9ef1b2cdb45
Filip Vrba <filipvrba@pm.me>
2023-02-28 07:36:49 +0100

    update env

--- src/.js/main.js
+++ src/.js/main.js
@@ -2,4 +2,4 @@ import "../css/style.css";
 import { ENV } from "./env";
 document.querySelector("#app").innerHTML = "<h1>Hello RubyJS</h1>";
 console.log(`Token A: ${ENV.VITE_TOKEN_A}`);
-console.log(`Token B: ${ENV.VITE_TOKEN_B}`)
+console.log(`Token B: ${ENV.TOKEN_B}`)
