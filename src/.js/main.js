1a58e8704d268df18cca435bada171ee7a601d5c
Filip Vrba <filipvrba@pm.me>
2023-02-28 07:36:04 +0100

    test token via vite

---
+++ src/.js/main.js
@@ -0,0 +1,5 @@
+import "../css/style.css";
+import { ENV } from "./env";
+document.querySelector("#app").innerHTML = "<h1>Hello RubyJS</h1>";
+console.log(`Token A: ${ENV.VITE_TOKEN_A}`);
+console.log(`Token B: ${ENV.VITE_TOKEN_B}`)
